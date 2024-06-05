7c6f910a104ca56f51a60732bd1d3ff4b7a09db5
Conrad Ludgate <conradludgate@gmail.com>
2024-06-05 21:59:28 +0200

    add some safety comments

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ use arrayvec::DetachedArrayVec;
 
 mod arrayvec;
 
-// const M: usize = 8;
-const M: usize = 2;
+const M: usize = 8;
+// const M: usize = 2;
 
 impl<T, const M: usize> Children<T, M> {
     const fn new() -> Self {
@@ -24,6 +24,8 @@ struct NodeArray<T, const M: usize> {
 }
 
 impl<T, const M: usize> NodeArray<T, M> {
+    /// # Safety
+    /// height must be correct.
     unsafe fn drop_inner(&mut self, height: usize) {
         if std::mem::needs_drop::<T>() {
             self.pivots.clear(self.len);
@@ -46,6 +48,11 @@ struct Children<T, const M: usize> {
 }
 
 impl<T: Ord, const M: usize> NodeArray<T, M> {
+    const __M_IS_GREATER_THAN_ONE: bool = {
+        assert!(M > 2, "The fanout factor, M, must be greater than one");
+        true
+    };
+
     #[cold]
     fn rotate(
         &mut self,
@@ -56,6 +63,12 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
         debug_assert_eq!(self.len, M);
         debug_assert!(M >= 2);
 
+        let m2 = M / 2;
+        let m2m1 = m2 - 1;
+        let m2p1 = m2 + 1;
+        assert!(m2p1 < M);
+        assert!(m2 > 0);
+
         // we are creating a new node,
         // the values being split off from rhs will be written here.
         let mut new_node = NodeArray {
@@ -64,50 +77,49 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
             children: Children::<T, M>::new(),
         };
 
-        let mid = match usize::cmp(&index, &(M / 2)) {
+        let mid = match usize::cmp(&index, &m2) {
             std::cmp::Ordering::Equal => unsafe {
-                new_node.pivots = self.pivots.split_off(M, M / 2);
+                // SAFETY: M pivots are init. m2 < M.
+                new_node.pivots = self.pivots.split_off(M, m2);
 
                 if let Some(child) = child {
                     new_node.children.head.write(child);
-                    new_node.children.tail = self.children.tail.split_off(M, M / 2);
+                    // SAFETY: M children are init. m2 < M.
+                    new_node.children.tail = self.children.tail.split_off(M, m2);
                 }
 
                 value
             },
             std::cmp::Ordering::Less => unsafe {
-                new_node.pivots = self.pivots.split_off(M, M / 2);
-                let mid = self.pivots.pop(M / 2);
-                self.pivots.insert(M / 2 - 1, index, value);
+                new_node.pivots = self.pivots.split_off(M, m2);
+                let mid = self.pivots.pop(m2);
+                self.pivots.insert(m2m1, index, value);
 
                 if let Some(child) = child {
-                    new_node.children.tail = self.children.tail.split_off(M, M / 2);
-                    new_node.children.head.write(self.children.tail.pop(M / 2));
-                    self.children.tail.insert(M / 2 - 1, index, child);
+                    new_node.children.tail = self.children.tail.split_off(M, m2);
+                    new_node.children.head.write(self.children.tail.pop(m2));
+                    self.children.tail.insert(m2m1, index, child);
                 }
 
                 mid
             },
             std::cmp::Ordering::Greater => unsafe {
-                let index = index - M / 2 - 1;
-                new_node.pivots = self.pivots.split_off(M, M / 2 + 1);
-                let mid = self.pivots.pop(M / 2 + 1);
-                new_node.pivots.insert(M / 2 - 1, index, value);
+                let index = index - m2p1;
+                new_node.pivots = self.pivots.split_off(M, m2p1);
+                let mid = self.pivots.pop(m2p1);
+                new_node.pivots.insert(m2m1, index, value);
 
                 if let Some(child) = child {
-                    new_node.children.tail = self.children.tail.split_off(M, M / 2 + 1);
-                    new_node
-                        .children
-                        .head
-                        .write(self.children.tail.pop(M / 2 + 1));
-                    new_node.children.tail.insert(M / 2 - 1, index, child);
+                    new_node.children.tail = self.children.tail.split_off(M, m2p1);
+                    new_node.children.head.write(self.children.tail.pop(m2p1));
+                    new_node.children.tail.insert(m2m1, index, child);
                 }
 
                 mid
             },
         };
-        self.len = M / 2;
-        new_node.len = M / 2;
+        self.len = m2;
+        new_node.len = m2;
         InsertResult::Propagate {
             pivot: mid,
             right: Box::new(new_node),
@@ -115,6 +127,8 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
     }
 
     fn insert(&mut self, mut value: T, height: usize) -> InsertResult<T, M> {
+        assert!(Self::__M_IS_GREATER_THAN_ONE);
+
         // SAFETY: `len` pivots are init
         let pivots = unsafe { self.pivots.as_mut_slice(self.len) };
 
@@ -132,7 +146,9 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
             debug_assert!(self.len > 0, "non leaf nodes must have some children");
 
             let child = match index.checked_sub(1) {
+                // SAFETY: head is always init when height > 0
                 None => unsafe { self.children.head.assume_init_mut() },
+                // SAFETY: len children are init
                 Some(index) => unsafe { &mut self.children.tail.as_mut_slice(self.len)[index] },
             };
 
@@ -148,6 +164,9 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
         if self.len == M {
             self.rotate(index, value, new_child)
         } else {
+            // SAFETY:
+            // * len children and pivots are currently init
+            // * len is less than cap.
             unsafe {
                 self.pivots.insert(self.len, index, value);
                 if let Some(child) = new_child {
@@ -185,6 +204,7 @@ impl<T: std::fmt::Debug> std::fmt::Debug for OkBTree<T> {
 impl<T> Drop for OkBTree<T> {
     fn drop(&mut self) {
         if let Some(mut inner) = self.0.take() {
+            // SAFETY: height is set correctly.
             unsafe { inner.node.drop_inner(inner.depth.get() - 1) }
         }
     }
@@ -199,16 +219,21 @@ impl<T: std::fmt::Debug, const M: usize> std::fmt::Debug for NodeArrayFmt<'_, T,
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut list = f.debug_list();
 
+        // SAFETY: len pivots are init
         let pivots = unsafe { self.array.pivots.as_slice(self.array.len) };
 
         if self.height == 0 {
             list.entries(pivots);
-        } else if self.array.len > 0 {
+        } else {
+            debug_assert_ne!(self.array.len, 0);
+
             list.entry(&NodeArrayFmt {
                 height: self.height - 1,
+                // SAFETY: head is always init when height > 0
                 array: unsafe { self.array.children.head.assume_init_ref() },
             });
 
+            // SAFETY: len children are init
             let tail = unsafe { self.array.children.tail.as_slice(self.array.len) };
             for (p, c) in std::iter::zip(pivots, tail) {
                 list.entry(p);
@@ -248,6 +273,9 @@ impl<T: Ord> OkBTree<T> {
                         children: Children::new(),
                     };
 
+                    // SAFETY:
+                    // pivots and children are currently uninit.
+                    // M > 1 so there is capacity available.
                     unsafe {
                         node.pivots.push(0, pivot);
                         node.children.head.write(inner.node);
@@ -265,6 +293,9 @@ impl<T: Ord> OkBTree<T> {
             }
         } else {
             let mut pivots = DetachedArrayVec::new();
+            // SAFETY:
+            // pivots is currently uninit.
+            // M > 1 so there is capacity available.
             unsafe { pivots.push(0, value) };
             self.0 = Some(BTreeInner {
                 depth: NonZeroUsize::new(1).unwrap(),
