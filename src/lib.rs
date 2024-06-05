79f7a106ba522ff494322c259bfa6d4e3a5d79c9
Conrad Ludgate <conradludgate@gmail.com>
2024-06-05 08:16:24 +0200

    finish

--- src/lib.rs
+++ src/lib.rs
@@ -27,28 +27,30 @@ impl<T, const M: usize> Children<T, M> {
     }
 }
 
-// impl<T, const M: usize> std::ops::Deref for Children<T, M> {
-//     type Target = [MaybeUninit<Box<NodeArray<T, M>>>];
-
-//     fn deref(&self) -> &Self::Target {
-//         unsafe {
-//             &*std::ptr::slice_from_raw_parts(self as *const _ as *const MaybeUninit<_>, M + 1)
-//         }
-//     }
-// }
-// impl<T, const M: usize> std::ops::DerefMut for Children<T, M> {
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         unsafe {
-//             &mut *std::ptr::slice_from_raw_parts_mut(self as *mut _ as *mut MaybeUninit<_>, M + 1)
-//         }
-//     }
-// }
-
 struct NodeArray<T, const M: usize> {
     len: usize,
     pivots: [MaybeUninit<T>; M],
     // empty if height = 0
     children: Children<T, M>,
+}
+
+impl<T, const M: usize> NodeArray<T, M> {
+    fn drop_inner(&mut self, height: usize) {
+        if std::mem::needs_drop::<T>() {
+            for p in &mut self.pivots[..self.len] {
+                unsafe { p.assume_init_drop() }
+            }
+        }
+        if height > 0 {
+            let mut head = *unsafe { self.children.head.assume_init_read() };
+            head.drop_inner(height - 1);
+
+            for c in &mut self.children.tail[..self.len] {
+                let mut head = *unsafe { c.assume_init_read() };
+                head.drop_inner(height - 1);
+            }
+        }
+    }
 }
 
 // #[repr(C)]
@@ -88,22 +90,55 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
         let mut lhs = self.pivots.as_mut_ptr().cast::<T>();
         let rhs = unsafe { self.pivots.as_ptr().add(M / 2).cast::<T>() };
 
-        // we are creating a new leaf node,
+        // // children is a full [Box<NodeArray<T, M>>; M+1]
+        // // iff child is not none.
+        // let children = &mut self.children as *mut _ as *mut Box<NodeArray<T, M>>;
+
+        // we are creating a new node,
         // the values being split off from rhs will be written here.
-        let mut pivots = uninit_array::<T, M>();
-        let mut out = pivots[..M / 2].as_mut_ptr().cast::<T>();
+        let mut new_node = NodeArray {
+            len: 0,
+            pivots: uninit_array::<T, M>(),
+            children: Children::<T, M>::uninit(),
+        };
+
+        let mut out_p = new_node.pivots.as_mut_ptr().cast::<T>();
+        // let mut out_c = &mut new_node.children as *mut _ as *mut Box<NodeArray<T, M>>;
 
         let mid = match usize::cmp(&index, &(M / 2)) {
             std::cmp::Ordering::Equal => {
                 // in this case, lhs < value < rhs
                 // write rhs to out.
-                unsafe { rhs.copy_to_nonoverlapping(out, M / 2) }
+                unsafe { rhs.copy_to_nonoverlapping(out_p, M / 2) }
+
+                if let Some(child) = child {
+                    unsafe {
+                        new_node.children.head.write(Box::new(child));
+                        let out = new_node.children.tail.as_mut_ptr();
+                        let inp = self.children.tail.as_mut_ptr().add(M / 2);
+                        inp.copy_to_nonoverlapping(out, M / 2);
+                    }
+                }
+
                 value
             }
             std::cmp::Ordering::Less => {
                 // in this case, value < rhs
                 // write rhs to out.
-                unsafe { rhs.copy_to_nonoverlapping(out, M / 2) }
+                unsafe { rhs.copy_to_nonoverlapping(out_p, M / 2) }
+
+                if child.is_some() {
+                    unsafe {
+                        let inp = self.children.tail.as_mut_ptr().add(M / 2);
+
+                        new_node
+                            .children
+                            .head
+                            .write(inp.sub(1).read().assume_init());
+                        let out = new_node.children.tail.as_mut_ptr();
+                        inp.copy_to_nonoverlapping(out, M / 2);
+                    }
+                }
 
                 // let (lhs1, lhs2) = lhs.split_at(index);
                 // lhs1.len() == index;
@@ -115,13 +150,31 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
                 // let (mid, lhs2) = lhs2.split_last();
                 // lhs2.len() == M/2 - index - 1;
                 let mid = unsafe { lhs2.read() };
-                let lhs2 = unsafe { lhs2.add(M / 2 - index - 1) };
+                // let lhs2 = unsafe { lhs2.add(M / 2 - index - 1) };
 
                 // lhs = lhs1 || value || lhs2.
                 unsafe {
                     lhs = lhs.add(index);
-                    lhs2.copy_to_nonoverlapping(lhs.add(1), M / 2 - index - 1);
+                    lhs2.copy_to(lhs.add(1), M / 2 - index - 1);
                     lhs.write(value);
+                }
+
+                if let Some(child) = child {
+                    // [Box<NodeArray<T, M>>; M/2 - 1]
+                    let mut lhs = self.children.tail.as_mut_ptr() as *mut Box<NodeArray<T, M>>;
+
+                    // let (lhs1, lhs2) = lhs.split_at(index);
+                    // lhs1.len() == index;
+                    // lhs2.len() == M/2 - index - 1;
+                    // let lhs1 = lhs;
+                    let lhs2 = unsafe { lhs.add(index) };
+
+                    // lhs = lhs1 || child || lhs2.
+                    unsafe {
+                        lhs = lhs.add(index);
+                        lhs2.copy_to(lhs.add(1), M / 2 - index - 1);
+                        lhs.write(Box::new(child));
+                    }
                 }
 
                 mid
@@ -144,27 +197,57 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
 
                 // out = rhs1 || value || rhs2
                 unsafe {
-                    rhs1.copy_to_nonoverlapping(out, index - 1);
-                    out = out.add(index - 1);
+                    rhs1.copy_to_nonoverlapping(out_p, index - 1);
+                    out_p = out_p.add(index - 1);
 
-                    out.write(value);
-                    out = out.add(1);
+                    out_p.write(value);
+                    out_p = out_p.add(1);
 
-                    rhs2.copy_to_nonoverlapping(out, M / 2 - index);
+                    rhs2.copy_to_nonoverlapping(out_p, M / 2 - index);
+                }
+
+                if let Some(child) = child {
+                    // new_node.children.tail: [Box<NodeArray<T, M>>; M]
+                    let tail = self.children.tail.as_mut_ptr() as *mut Box<NodeArray<T, M>>;
+                    // rhs: [Box<NodeArray<T, M>>; M/2]
+                    let rhs = unsafe { tail.add(M / 2) };
+
+                    // let (rhs1, rhs2) = rhs.split_at(index);
+                    // rhs1.len() == index;
+                    // rhs2.len() == M/2-index;
+                    let rhs1 = rhs;
+                    let rhs2 = unsafe { rhs.add(index) };
+
+                    // first value of rhs1 is the head
+                    // let (head, rhs1) = rhs1.split_first();
+                    // rhs1.len() == index - 1;
+                    let head = unsafe { rhs1.read() };
+                    let rhs1 = unsafe { rhs1.add(1) };
+
+                    // out = rhs1 || child || rhs2
+                    let mut out_p =
+                        new_node.children.tail.as_mut_ptr() as *mut Box<NodeArray<T, M>>;
+                    unsafe {
+                        new_node.children.head.write(head);
+
+                        rhs1.copy_to_nonoverlapping(out_p, index - 1);
+                        out_p = out_p.add(index - 1);
+
+                        out_p.write(Box::new(child));
+                        out_p = out_p.add(1);
+
+                        rhs2.copy_to_nonoverlapping(out_p, M / 2 - index);
+                    }
                 }
 
                 mid
             }
         };
         self.len = M / 2;
+        new_node.len = M / 2;
         InsertResult2::Propagate {
             pivot: mid,
-            right: NodeArray {
-                len: M / 2,
-                pivots,
-                // new leaf has no children
-                children: Children::uninit(),
-            },
+            right: new_node,
         }
     }
 
@@ -201,8 +284,7 @@ impl<T: Ord, const M: usize> NodeArray<T, M> {
             match child.insert(value, height - 1) {
                 InsertResult2::Propagate { pivot, right } => {
                     if self.len == M {
-                        todo!()
-                        // self.rotate(index, pivot, Some(right))
+                        self.rotate(index, pivot, Some(right))
                     } else {
                         // pivots: [T; self.len]
                         let pivots = self.pivots.as_mut_ptr().cast::<T>();
@@ -246,6 +328,14 @@ impl<T: std::fmt::Debug> std::fmt::Debug for OkBTree<T> {
             .fmt(f)
         } else {
             f.debug_list().finish()
+        }
+    }
+}
+
+impl<T> Drop for OkBTree<T> {
+    fn drop(&mut self) {
+        if let Some(mut inner) = self.0.take() {
+            inner.node.drop_inner(inner.depth.get() - 1)
         }
     }
 }
